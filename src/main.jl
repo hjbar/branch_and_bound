21ee37b7202440cea69fdfcd8f2e3761cc409f27
hjbar <hjbar@protonmail.com>
2024-04-26 16:26:04 +0200

    add some tests

--- src/main.jl
+++ src/main.jl
@@ -29,9 +29,15 @@ function test_instance(file)
 end
 
 function main()
-    test_instance("test/instance_test.dat")
+    # test_instance("test/instance_test.dat") # test solution exacte
 
-    test_instance("instances/sac94/pb/pb2.dat")
+    # test_instance("instances/sac94/pb/pb2.dat")
+
+    # test_instance("test/instance_random_50.dat") # test efficacité branch_and_bound
+
+    # test_instance("test/instance_random_1000.dat") # test efficacité branch_and_bound
+
+    test_instance("test/instance_random_50000.dat") # test efficacité branch_and_bound
 
     # test_instance("instances/sac94/pb/pb4.dat") # cas où glouton optimal
 
--- test/generate_input.ml
+++ test/generate_input.ml
@@ -2,12 +2,12 @@ let c = 1000
 let _ = Random.self_init()
 
 let generate nvar =
-  Printf.sprintf "%i 1 %i" nvar (Random.int c)
+  Printf.sprintf "%i 1 %i" nvar 0
   (* objectif *)
-  ^ (List.init nvar (fun _ -> Printf.sprintf "%i " (Random.int c)) |> List.fold_left (^) "\n")
+  ^ (List.init nvar (fun _ -> Printf.sprintf "%i " (c + Random.int c)) |> List.fold_left (^) "\n")
   (* contrainte *)
   ^ (List.init nvar (fun _ -> Printf.sprintf "%i " (Random.int c)) |> List.fold_left (^) "\n")
   (* poid *)
-  ^ (Printf.sprintf "\n%i" (Random.int c+500))
+  ^ (Printf.sprintf "\n%i" (c / 2 + Random.int c))
 
-let _ = generate 1000 |> Printf.printf "%s"
+let _ = generate 50000 |> Printf.printf "%s"
